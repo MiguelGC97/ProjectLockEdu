8e17cc7c795f46084144a5ce095fbcfa189b17f1
Miguel Ángel Gutiérrez Carreño <miguelangelgutierrezcarreno@alumno.ieselrincon.es>
2024-10-30 17:22:48 +0000

    create function added to controller

---
+++ backend/controllers/box.controller.js
@@ -0,0 +1,49 @@
+const db = require("../models");
+const Box = db.box;
+
+exports.create = (req, res) => {
+
+    const box = {
+        description: req.body.description,
+        imgUrl: req.body.color,
+    };
+
+    Box.create(box)
+        .then(data => {
+            res.send(data);
+        })
+        .catch(err => {
+            res.status(500).send({
+                message:
+                    err.message || "An error ocurred while creating the box"
+            });
+        });
+};
+
+
+exports.findAll = (req, res) => {
+
+
+
+};
+
+
+
+exports.findAllQuery = (req, res) => {
+
+    
+
+};
+
+
+exports.update = (req, res) => {
+
+    
+
+};
+
+
+exports.delete = (req, res) => {
+
+    
+};
