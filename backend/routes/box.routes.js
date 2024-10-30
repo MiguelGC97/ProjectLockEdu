e146b8d68017935451516979a197a3fe3b7e42da
Miguel Ángel Gutiérrez Carreño <miguelangelgutierrezcarreno@alumno.ieselrincon.es>
2024-10-30 18:36:27 +0000

    box routes created

---
+++ backend/routes/box.routes.js
@@ -0,0 +1,17 @@
+module.exports = app => {
+    const boxes = require("../controllers/box.controller.js");
+
+    var router = require("express").Router();
+
+    router.post("/", boxes.create);
+
+    router.get("/", boxes.findAll);
+
+    router.get("/:id", boxes.findOne);
+
+    router.put("/:id", boxes.update);
+
+    router.delete("/:id", boxes.delete);
+
+    app.use("/api/boxes", router);
+}
