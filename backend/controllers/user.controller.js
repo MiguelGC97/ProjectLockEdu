7276a640f46f07c2157a6b387e50e9551aecd87e
Yamiley <thed4ydr3amer@gmail.com>
2024-10-31 17:29:23 +0100

    crud working

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -37,25 +37,30 @@ userController.addNewUser = async (req, res) => {
 };
 
 userController.delete = async (req, res) => {
-    const deleting = await User.destroy({where: {id: req.body.id} });
+    const deleting = await User.destroy({where: {id: req.params.id} });
     const status = deleting ? 200:404; //operador ternario.  condición ? valor_si_verdadero : valor_si_falso
     const message = deleting ? "User deleted" : "User not found";
     res.status(status).json({ message });
 };
 
-//FALTA IMPLEMENTAR EL UPDATE
-// userController.update = async (req, res) => {
-//     try {
-//         const [updated] = await User.update(req.body, { where: { id: req.body.id } });
-//         if (updated) {
-//             const updatedUser = await User.findOne({ where: { id: req.body.id } });
-//             res.status(200).json({ message: "Usuario actualizado", data: updatedUser });
-//         } else {
-//             res.status(404).json({ message: "Usuario no encontrado" });
-//         }
-//     } catch (error) {
-//         res.status(500).json({ error: error.message });
-//     }
-// };
+
+userController.update = async (req, res) => {
+    try {
+        const id = req.params.id; 
+
+        const [updated] = await User.update(req.body, { where: { id } });
+
+        if (updated) {
+            res.status(200).json({
+                message: "Usuario actualizado",
+                data: req.body,
+            });
+        } else {
+            res.status(404).json({ message: "Usuario no encontrado" });
+        }
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+};
 
 module.exports = userController;
