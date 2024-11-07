f5f8773318efbbf87be3c07ecc589a0f6ae30030
MiguelGC97 <miguelangelgutierrezcarreno@alumno.ieselrincon.es>
2024-11-07 19:03:34 +0000

    type model created

---
+++ backend/models/type.model.js
@@ -0,0 +1,18 @@
+﻿const { DataTypes } = require('sequelize');
+
+module.exports = (sequelize) => {
+    const Type = sequelize.define('types', {
+        id: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            primaryKey: true
+        },
+        typeName: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+    });
+
+    return Type;
+}
+
