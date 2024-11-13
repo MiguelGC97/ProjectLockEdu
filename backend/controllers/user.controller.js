ff85e7be445389ff245c0ab259345689a85b0398
Yamiley <thed4ydr3amer@gmail.com>
2024-11-13 20:40:06 +0100

    Backend important changes- Index changes and Db connection changes

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,67 +1,63 @@
-
 const { Sequelize } = require("sequelize");
-const User = require ("../models/user.model");
+const User = require("../models/user.model");
 const { Op } = require("sequelize");
+const utils = require("../utils");
+const  bcrypt  =  require('bcryptjs');
 
-const userController = {};
+exports.getAll = async (req, res) => {
+  try {
+    const users = await User.findAll();
+    res.status(200).json({ data: users });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
 
-userController.getAll = async(req, res) => {
-    try{
-        const users = await User.findAll();
-        res.status(200).json({ data:users });
-    }catch(error){
-        res.status(500).json({ error: error.message});
+exports.getByUsername = async (req, res) => {
+  try {
+    const q = req.params.username;
+    const users = await User.findAll({
+      where: { username: { [Op.like]: "%" + q + "%" } },
+    });
+    res.status(200).json({ data: users });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+exports.addNewUser = async (req, res) => {
+  try {
+    const users = await User.create(req.body);
+    res.status(201).json({ data: users });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+exports.delete = async (req, res) => {
+  const deleting = await User.destroy({ where: { id: req.params.id } });
+  const status = deleting ? 200 : 404; //operador ternario.  condición ? valor_si_verdadero : valor_si_falso
+  const message = deleting ? "User deleted" : "User not found";
+  res.status(status).json({ message });
+};
+
+exports.update = async (req, res) => {
+  try {
+    const id = req.params.id;
+
+    const [updated] = await User.update(req.body, { where: { id } });
+
+    if (updated) {
+      res.status(200).json({
+        message: "Usuario actualizado",
+        data: req.body,
+      });
+    } else {
+      res.status(404).json({ message: "Usuario no encontrado" });
     }
-};
-
-userController.getByUsername = async (req, res) => {
-    try{
-        const q = req.params.username;
-        const users = await User.findAll({
-            where: 
-            { username:  {[Op.like]: '%'+ q + '%'}} ,
-        });
-        res.status(200).json({ data: users });
-        
-    }catch(error) {
-        res.status(500).json({ error: error.message});
-    }
-};
-
-userController.addNewUser = async (req, res) => {
-    
-    try{const users = await User.create(req.body);
-    res.status(201).json({ data:users});
-}catch(error) {
-    res.status(500).json({error: error.message});
-}
-};
-
-userController.delete = async (req, res) => {
-    const deleting = await User.destroy({where: {id: req.params.id} });
-    const status = deleting ? 200:404; //operador ternario.  condición ? valor_si_verdadero : valor_si_falso
-    const message = deleting ? "User deleted" : "User not found";
-    res.status(status).json({ message });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 };
 
 
-userController.update = async (req, res) => {
-    try {
-        const id = req.params.id; 
-
-        const [updated] = await User.update(req.body, { where: { id } });
-
-        if (updated) {
-            res.status(200).json({
-                message: "Usuario actualizado",
-                data: req.body,
-            });
-        } else {
-            res.status(404).json({ message: "Usuario no encontrado" });
-        }
-    } catch (error) {
-        res.status(500).json({ error: error.message });
-    }
-};
-
-module.exports = userController;
--- backend/models/index.js
+++ backend/models/index.js
@@ -1,23 +1,37 @@
-﻿const dbConfig = require("../config/db.config.js");
-const Sequelize = require("sequelize");
-const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
-    host: dbConfig.HOST,
-    dialect: dbConfig.dialect,
-    operatorAliases: false,
+﻿'use strict';
 
-    pool: dbConfig.pool
-});
-
+const fs = require('fs');  //D--imports archive system module  who exist to interact with the archive system we use it in the filter
+const path = require('path');
+const Sequelize = require('sequelize');
+const basename = path.basename(__filename);
+const env = process.env.NODE_ENV || 'development';
+const config = require(__dirname + '/../config/config.js')[env];
 const db = {};
 
-db.Sequelize = Sequelize;
+let sequelize;
+if (config.use_env_variable) {
+  sequelize = new Sequelize(process.env[config.use_env_variable], config);
+} else {
+  sequelize = new Sequelize(config.database, config.username, config.password, config);
+}
+
+fs //D--
+  .readdirSync(__dirname)
+  .filter(file => {
+    return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
+  })
+  .forEach(file => {
+    const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
+    db[model.name] = model;
+  });
+
+Object.keys(db).forEach(modelName => {
+  if (db[modelName].associate) {
+    db[modelName].associate(db);
+  }
+});
+
 db.sequelize = sequelize;
-
-db.locker = require("./locker.model.js")(sequelize, Sequelize);
-db.box = require("./box.model.js")(sequelize, Sequelize);
-
-// db.user = require("./user.model.js")(sequelize, Sequelize);
-
-// db.user = require("./object.model.js")(sequelize, Sequelize);
+db.Sequelize = Sequelize;
 
 module.exports = db;
