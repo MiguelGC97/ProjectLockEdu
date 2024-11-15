6c6b04073458083fdb750d6fcb670ce5dddb77a0
Yamiley <thed4ydr3amer@gmail.com>
2024-11-15 18:55:20 +0100

    error access denied sequelize

--- backend/models/index.js
+++ backend/models/index.js
@@ -1,23 +1,56 @@
 ﻿const dbConfig = require("../config/db.config.js");
 const Sequelize = require("sequelize");
-const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
-    host: dbConfig.HOST,
-    dialect: dbConfig.dialect,
-    operatorAliases: false,
+const { Op } = require("sequelize");
+// const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
+//     host: dbConfig.HOST,
+//     dialect: dbConfig.dialect,
+//     operatorAliases: false,
 
-    pool: dbConfig.pool
-});
+//     pool: dbConfig.pool
+// });
 
 const db = {};
+
+
+let sequelize;
+if (config.use_env_variable) {
+  sequelize = new Sequelize(process.env[config.use_env_variable], config);
+} else {
+  sequelize = new Sequelize(config.database, config.username, config.password, config);
+}
+
 
 db.Sequelize = Sequelize;
 db.sequelize = sequelize;
 
 db.locker = require("./locker.model.js")(sequelize, Sequelize);
 db.box = require("./box.model.js")(sequelize, Sequelize);
+// db.type = require("./type.model.js")(sequelize, Sequelize);
+// db.item = require("./item.model.js")(sequelize, Sequelize);
+db.user = require("./user.model.js")(sequelize, Sequelize);
 
-// db.user = require("./user.model.js")(sequelize, Sequelize);
+// Locker.hasMany(Box);
+// Box.belongsTo(Locker);
 
-// db.user = require("./object.model.js")(sequelize, Sequelize);
+//Box.hasMany(Item);
+//Item.belongsTo(Box);
+
+//Box.hasMany(Incident);
+//Incident.belongsTo(Box);
+
+//Type.hasMany(Item);
+//Item.belongsTo(Type);
+
+//User.hasMany(Incident);
+//Incident.belongsTo(User);
+
+//User.hasMany(Booking);
+//Booking.belongsTo(User);
+
+//Item.belongsToMany(Booking, { through: 'ItemBookings' /* options */ });
+//Booking.belongsToMany(Item, { through: 'ItemBookings' /* options */ });
+
+//User.belongsToMany(Incident, { thorugh: 'UserIncidents' /* options */ });
+//Incident.belongsToMany(User, { thorugh: 'UserIncidents' /* options */ });
 
 module.exports = db;
