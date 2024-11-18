025342fab1c9ff25d97b3929457182d25837c1f9
MiguelGC97 <miguelangelgutierrezcarreno@alumno.ieselrincon.es>
2024-11-18 09:27:29 +0000

    box controller updated

--- backend/controllers/box.controller.js
+++ backend/controllers/box.controller.js
@@ -1,103 +1,76 @@
 const db = require("../models");
 const Box = db.box;
 
-// exports.create = (req, res) => {
+exports.addBox = async (req, res) => {
+  try {
+    const boxes = await Box.create(req.body);
+    res.status(201).json({ data: boxes });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
 
-//     const box = {
-//         id: req.body.id,
-//         description: req.body.description,
-//         imgUrl: req.body.color,
-//     };
+exports.getAll = async (req, res) => {
+  try {
+    const boxes = await Box.findAll();
+    res.status(200).json({ data: boxes });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
 
-//     Box.create(box)
-//         .then(data => {
-//             res.send(data);
-//         })
-//         .catch(err => {
-//             res.status(500).send({
-//                 message:
-//                     err.message || "An error ocurred while creating the box"
-//             });
-//         });
+exports.getOne = async (req, res) => {
+  try {
+    const id = req.params.id;
+    const boxes = await Box.findByPk(id);
+
+    if (!box) {
+      return res.status(404).json({ error: "Box not found" });
+    }
+
+    res.status(200).json({ data: boxes });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+exports.update = async (req, res) => {
+  try {
+    const id = req.params.id;
+
+    const [updated] = await Box.update(req.body, { where: { id } });
+
+    if (updated) {
+      res.status(200).json({
+        message: "Box updated",
+        data: req.body,
+      });
+    } else {
+      res.status(404).json({ message: "Box not found" });
+    }
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+// exports.delete = (req, res) => {
+//   const id = req.params.id;
+
+//   Box.destroy({ where: { id: id } })
+//     .then(() => {
+//       console.log("Box erased");
+//       res.send({ message: "Box erased" });
+//     })
+//     .catch((err) => {
+//       res.status(500).send({
+//         message: err.message || "Some error ocurred while deleting the box",
+//       });
+//     });
 // };
 
-exports.addBox = async (req, res) => {
-    try {
-        const boxes = await Box.create(req.body);
-        res.status(201).json({ data: boxes })
-    } catch (error) {
-        res.status(500).json({ error: error.message });
-    }
-};
-
-exports.findAll = (req, res) => {
-    Box.findAll()
-        .then((data) => {
-            res.send(data);
-        })
-        .catch(err => {
-            res.status(500).send({
-                message:
-                    err.message || "An error ocurred while retrieving the boxes"
-            });
-    });
-};
-
-exports.findOne = (req, res) => {
-    const id = req.params.id;
-
-    Box.findByPk(id)
-        .then(data => {
-            if (!data) {
-                res.status(404).send({ message: "Box not found." });
-            } else {
-                res.send(data);
-            }
-        })
-        .catch(err => {
-            res.status(500).send({
-                message: err.message || "An error ocurred while retrieving the box"
-            });
-        })
-};
-
-exports.findAllQuery = (req, res) => {
-
-    
-
-};
-
-
-exports.update = (req, res) => {
-    const id = req.params.id;
-
-    if (!id) {
-        return res.status(400).send({
-            message: "Not a valid ID"
-        });
-    }
-    
-    Box.update(req.body, { where: {id: id}})
-    .then(() => {
-        console.log("Box updated");
-        res.send({message: "Box updated"});
-    })
-};
-
-
-exports.delete = (req, res) => {
-    const id = req.params.id;
-
-    Box.destroy({ where: { id: id}})
-    .then(() => {
-        console.log("Box erased");
-        res.send({message: "Box erased"});
-    })
-    .catch(err => {
-        res.status(500).send({
-            message:
-                err.message || "Some error ocurred while deleting the box"
-        });
-    });
-    
+exports.delete = async (req, res) => {
+  const deleting = await Box.destroy({ where: { id: req.params.id } });
+  const status = deleting ? 200 : 404;
+  const message = deleting ? "Box deleted" : "Box not found";
+  res.status(status).json({ message });
 };
