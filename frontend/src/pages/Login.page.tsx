9a6b16324378ab55da1c352c979a911cc6043c8e
scsoares <sarahcsf2@gmail.com>
2024-12-17 16:06:54 +0000

    update login page

--- frontend/src/pages/Login.page.tsx
+++ frontend/src/pages/Login.page.tsx
@@ -21,7 +21,7 @@ import '../App.module.css';
 const Login: React.FC = () => {
   return (
     <Flex maw="100%">
-      <Flex maw="55%" height="100vh">
+      <Flex maw="55%" h="100vh">
         <img
           className="loginImage"
           src="https://images.unsplash.com/photo-1553799296-2da61db672c7?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
