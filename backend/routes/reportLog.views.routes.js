68bffb6ef494e96a4624e729ee7a5e55bfaf55e8
Yamiley <thed4ydr3amer@gmail.com>
2025-01-17 15:49:01 +0100

    refactor reportLog views: clean up routes, remove unused edit view, and add comments to templates

--- backend/routes/reportLog.views.routes.js
+++ backend/routes/reportLog.views.routes.js
@@ -1,18 +1,17 @@
 module.exports = (app) => {
-    
   const reportLog = require("../controllers/reportLog.views.controller.js");
 
   var router = require("express").Router();
 
-  // router.post("/", reportLog.store);
+  router.post("/", reportLog.store);
 
   router.get("/", reportLog.index);
 
-  // router.get("/create", reportLog.create);
+  router.get("/create", reportLog.create);
 
-  // router.get("/:id", reportLog.edit);
+  router.get("/:id", reportLog.edit);
 
-  // router.post("/:id", reportLog.update);
+  router.post("/:id", reportLog.update);
 
   app.use("/reportlog", router);
 };
