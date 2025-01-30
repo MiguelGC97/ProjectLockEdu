c5d0747f1388a1aa2c9e4c92a08168b156557047
Yamizs <yamihego99gmail.com>
2025-01-30 13:49:32 +0000

    feat: add user authentication routes and views; secure report log routes

--- backend/routes/reportLog.views.routes.js
+++ backend/routes/reportLog.views.routes.js
@@ -1,17 +1,17 @@
 module.exports = (app) => {
   const reportLog = require("../controllers/reportLog.views.controller.js");
-
+const authSession = require("../middlewares/auth.session.js");
   var router = require("express").Router();
 
-  router.post("/", reportLog.store);
+  router.post("/", authSession.isAuthenticated, reportLog.store);
 
-  router.get("/", reportLog.index);
+  router.get("/",  authSession.isAuthenticated, reportLog.index);
 
-  router.get("/create", reportLog.create);
+  router.get("/create", authSession.isAuthenticated, breportLog.create);
 
-  router.get("/:id", reportLog.edit);
+  router.get("/:id", authSession.isAuthenticated, reportLog.edit);
 
-  router.post("/:id", reportLog.update);
+  router.post("/:id", authSession.isAuthenticated, reportLog.update);
 
   app.use("/reportLog", router);
 };
