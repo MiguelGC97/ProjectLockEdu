ed88e4b612ed9a553a992fb8d9f4851eb49adfdd
MiguelGC97 <miguelangelgutierrezcarreno@alumno.ieselrincon.es>
2025-01-30 17:21:03 +0000

    fixed notifications errors

--- backend/seeders/20241121192926-seed-notifications.js
+++ backend/seeders/20241121192926-seed-notifications.js
@@ -2,8 +2,8 @@
 module.exports = {
   async up(queryInterface) {
     await queryInterface.bulkInsert('notifications', [
-      { userId: '1', bookingId: 1, content: 'Ha hecho una nueva reserva', isRead: false, type: info, createdAt: new Date(), updatedAt: new Date() },
-      { userId: '1', bookingId: 1, content: 'Tiene una recogida próxima en 15 minutos', isRead: false, type: reminder, createdAt: new Date(), updatedAt: new Date() },
+      { userId: '1', bookingId: 1, content: 'Ha hecho una nueva reserva', isRead: false, type: 'info', createdAt: new Date(), updatedAt: new Date() },
+      { userId: '1', bookingId: 1, content: 'Tiene una recogida próxima en 15 minutos', isRead: false, type: 'reminder', createdAt: new Date(), updatedAt: new Date() },
     ], {});
   },
 
