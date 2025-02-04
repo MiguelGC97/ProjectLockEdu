68cc2d27b273b24b0cf1cae78b6fbf01816dafe0
Corny-atk <cornelis.attakpa05@gmail.com>
2025-02-04 19:13:53 +0100

    settings page

--- frontend/src/pages/Settings.page.tsx
+++ frontend/src/pages/Settings.page.tsx
@@ -1,20 +1,26 @@
 ﻿// Home.page.tsx
-import { Flex, useMantineTheme } from '@mantine/core';
+import { Avatar, Flex, useMantineTheme } from '@mantine/core';
 import { useMediaQuery } from '@mantine/hooks';
 import { Banner } from '@/components/Banner/Banner';
 import BookingHistoryBox from '@/components/BookingHistoryBox/BookingHistoryBox';
 import MobileMenu from '@/components/MobileMenu/MobileMenu';
 import { SideMenu } from '@/components/SideMenu/SideMenu';
 import UserBar from '@/components/UserBar/UserBar';
+import { useAuth } from '@/hooks/AuthProvider';
 
 const Settings: React.FC = () => {
   const theme = useMantineTheme();
   const matches = useMediaQuery('(min-width: 85em)');
   const matches2 = useMediaQuery('(max-width: 93em)');
+  const { user } = useAuth();
 
   return (
     <>
-      <div>Hola holita historial</div>
+      <SideMenu />
+      <Flex justify="flex-end">
+        <Avatar size="lg" src={user?.avatar} alt="User profile photo" bd="3px solid white" />
+      </Flex>
+      
     </>
   );
 };
