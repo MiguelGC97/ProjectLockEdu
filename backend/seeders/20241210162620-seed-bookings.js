648f600aacf947d2db0675fa9f308b1f6f5e45f1
MiguelGC97 <miguelangelgutierrezcarreno@alumno.ieselrincon.es>
2025-02-05 18:47:09 +0000

    reminder notification card logic almost done

--- backend/seeders/20241210162620-seed-bookings.js
+++ backend/seeders/20241210162620-seed-bookings.js
@@ -2,7 +2,7 @@ module.exports = {
   async up(queryInterface) {
     await queryInterface.bulkInsert('bookings', [
       { id: 1, description: 'Reserva 1', checkOut: "2024-12-10 14:00:00", checkIn: "2024-12-15 12:00:00", state: "pending", userId: 1, createdAt: new Date(), updatedAt: new Date() },
-      { id: 2, description: 'Reserva 2', checkOut: "2024-12-10 14:00:00", checkIn: "2024-12-15 12:00:00", state: "withdrawn", userId: 2, createdAt: new Date(), updatedAt: new Date() },
+      { id: 2, description: 'Reserva 2', checkOut: "2025-2-5 19:00:00", checkIn: "2025-2-5 19:00:00", state: "withdrawn", userId: 2, createdAt: new Date(), updatedAt: new Date() },
       { id: 3, description: 'Reserva 3', checkOut: "2024-12-10 14:00:00", checkIn: "2024-12-15 12:00:00", state: "pending", userId: 2, createdAt: new Date(), updatedAt: new Date() },
       { id: 4, description: 'Reserva 4', checkOut: "2024-12-10 14:00:00", checkIn: "2024-12-15 12:00:00", state: "pending", userId: 3, createdAt: new Date(), updatedAt: new Date() },
     ], {});
