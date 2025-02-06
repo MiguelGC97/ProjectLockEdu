6a7f88044599d2f02312a729fd34e902213d1dbd
Corny-atk <cornelis.attakpa05@gmail.com>
2025-02-06 21:34:17 +0100

    frontend settings changes

--- backend/seeders/20241121192927-seed-settings.js
+++ backend/seeders/20241121192927-seed-settings.js
@@ -2,7 +2,7 @@
 module.exports = {
   async up(queryInterface) {
     await queryInterface.bulkInsert('settings', [
-      { id: 1, theme: 'Dark', banner : 'test', notifications: true, createdAt: new Date(), updatedAt: new Date() },
+      { id: 1, theme: 'Dark', banner : 'test', notifications: true, userId: 1, createdAt: new Date(), updatedAt: new Date() },
     ], {});
   },
 
--- frontend/src/components/SettingsBox/SettingsBox.tsx
+++ frontend/src/components/SettingsBox/SettingsBox.tsx
@@ -1,7 +1,11 @@
-import { Box, Center, Divider } from "@mantine/core";
+import { Box, Center, Checkbox, Divider, Flex, Input, PasswordInput, Stack } from "@mantine/core";
 import classes from './SettingsBox.module.css';
+import { IconMessageReport, IconNotification, IconUser } from "@tabler/icons-react";
+import { useDisclosure } from "@mantine/hooks";
 
 export function SettingsBox(){
+    const [visible, { toggle }] = useDisclosure(false);
+    
     return(
         <>
         <Box bg="transparent" h="80vh" bd="1px solid myPurple.1" w="100%"  style={{
@@ -9,6 +13,53 @@ export function SettingsBox(){
         }} >
             <Center className={classes.banner}></Center>
             <Divider size="xs" color="myPurple.1" />
+                
+            <Input variant="unstyled" size="lg" placeholder="Mi perfil" leftSection={<IconUser size={25} color="white"/>} />
+            <Divider size="xs" color="myPurple.1" />
+
+            <Input variant="unstyled" size="lg" ml={60} placeholder="Cambiar contraseña" />
+            <Divider size="xs" color="myPurple.1" />
+
+            <Stack m={20} mb={50}>
+                <PasswordInput
+                    w={220}
+                    ml={100}
+                    label="Contraseña actual"
+                    styles={{
+                        label: {
+                        color: 'white',
+                        fontSize: '15px'
+                        }
+                    }}
+                    defaultValue="secret"
+                    visible={visible}
+                    onVisibilityChange={toggle}
+                />
+                <PasswordInput
+                    w={220}
+                    ml={100}
+                    styles={{
+                        label: {
+                        color: 'white',
+                        fontSize: '15px'
+                        }
+                    }}
+                    label="Nueva contraseña"
+                    defaultValue="secret"
+                    visible={visible}
+                    onVisibilityChange={toggle}
+                />
+            </Stack>
+            <Divider size="xs" color="myPurple.1" />
+
+            <Input variant="unstyled" size="lg" placeholder="Configuracion" leftSection={<IconMessageReport size={25} color="white"/>} />
+            <Divider size="xs" color="myPurple.1" />
+
+            <Checkbox
+                defaultChecked
+                label="I agree to sell my privacy"
+            />
+            
         </Box>
       
         </>
