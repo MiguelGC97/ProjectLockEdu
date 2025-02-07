d806fa37ad8bf896508ef24110e8a4b577ff885a
MiguelGC97 <miguelangelgutierrezcarreno@alumno.ieselrincon.es>
2025-02-07 20:32:20 +0000

    working on edit and delete functions

--- backend/controllers/notification.views.controller.js
+++ backend/controllers/notification.views.controller.js
@@ -40,79 +40,117 @@ exports.index = (req, res) => {
 };
 
 const findAll = (req, res) => {
-  Notification.findAll(
-    {
-      include: [
-        {
-          model: User,
-          attributes: ['name', 'surname', 'role'],
-        },
-        {
-          model: Booking,
-          attributes: ['id','checkOut', 'checkIn'],
-          include: [
-            {
-              model: Item,
-              attributes: ['id'],
-              include: [
-                {
-                  model: Box,
-                  attributes: ['id'],
-                  include: [
-                    {
-                      model: Locker,
-                      attributes: ['id'],
-                    },
-                  ],
-                },
-              ],
-            },
-          ],
-        },
-      ],
-    }
-  )
-  .then(data => {
+
+  const notificationsPromise = Notification.findAll({
+    include: [
+      {
+        model: User,
+        attributes: ['name', 'surname', 'role'],
+      },
+      {
+        model: Booking,
+        attributes: ['id','checkOut', 'checkIn'],
+        include: [
+          {
+            model: Item,
+            attributes: ['id'],
+            include: [
+              {
+                model: Box,
+                attributes: ['id'],
+                include: [
+                  {
+                    model: Locker,
+                    attributes: ['id'],
+                  },
+                ],
+              },
+            ],
+          },
+        ],
+      },
+    ],
+  });
+
+
+  const usersPromise = User.findAll({
+    attributes: ['id', 'name', 'surname', 'role'],
+    include: [
+      {
+        model: Booking,
+        attributes: ['id'],
+      }
+    ]
     
-    const now = new Date();
+  });
 
-    const notifications = data.map(notification => {
-      if (notification.booking && notification.booking.checkOut) {
-        const checkOut = new Date(notification.booking.checkOut);
-        const diffMs = checkOut - now;
 
-        if (diffMs < 0) {
+  Promise.all([notificationsPromise, usersPromise])
+    .then(([notificationsData, users]) => {  
+      const now = new Date();  
+
+      const notifications = notificationsData.map(notification => {
+        if (notification.booking && notification.booking.checkOut) {
+          const checkOut = new Date(notification.booking.checkOut);
+          const diffMs = checkOut - now;
+
+          if (diffMs < 0) {
+            return {
+              ...notification.toJSON(),
+              timeMessage: "Tiempo expirado"
+            };
+          }
+
+          const diffHours = Math.floor(diffMs / (1000 * 60 * 60));
+          const diffMinutes = Math.floor((diffMs % (1000 * 60 * 60)) / (1000 * 60));
+
           return {
             ...notification.toJSON(),
-            timeMessage: "Tiempo expirado"
+            diffHours,
+            diffMinutes,
+            timeMessage: `${diffHours} horas y ${diffMinutes} minutos`,
           };
         }
+        return { ...notification.toJSON(), diffHours: null, diffMinutes: null };
+      });
 
-        const diffHours = Math.floor(diffMs / (1000 * 60 * 60));
-        const diffMinutes = Math.floor((diffMs % (1000 * 60 * 60)) / (1000 * 60));
-
-        return {
-          ...notification.toJSON(), 
-          diffHours,
-          diffMinutes,
-          timeMessage: `${diffHours} horas y ${diffMinutes} minutos`,
-        };
-      }
-      return { ...notification.toJSON(), diffHours: null, diffMinutes: null };
+      return res.render('notifications/index', {
+        notifications,
+        users,
+        activeRoute: "notifications",
+        user: req.session.user,
+      });
+    })
+    .catch(err => {
+      return res.render("error", {
+        message: err.message || "Some error occurred while retrieving notifications."
+      });
     });
-
-    return res.render('notifications/index', { notifications, activeRoute: "notifications" });
-  })
-  .catch(err => {
-    return res.render("error", {
-      message: err.message || "Some error occurred while retrieving notifications."
-    });
-  });
 }
-
 
 exports.create = (req, res) => {
   return res.render("notifications/create");
 };
 
+exports.markAsRead = async (req, res) => {
+  const id = req.params.id;
 
+  try {
+    const [updated] = await Notification.update(
+      { isRead: true },
+      { where: { id } }
+    );
+
+    if (updated) {
+      return res.redirect("/notifications/index");
+    } else {
+      return res.render("error", {
+        message: `Could not mark as read the notification with id=${id}.`,
+      });
+    }
+  } catch (err) {
+    return res.render("error", {
+      message: `Error while trying to mark as read the notification with id=${id}.`,
+    });
+  }
+};
