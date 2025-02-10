42f41d7ac66887fc384b96f2af06d78b8d080bb7
Corny-atk <cornelis.attakpa05@gmail.com>
2025-02-10 18:32:47 +0100

    feat : settings Page, change password, add button

--- frontend/src/components/SettingsBox/SettingsBox.tsx
+++ frontend/src/components/SettingsBox/SettingsBox.tsx
@@ -1,25 +1,51 @@
-import { Box, Center, Checkbox, Divider, Flex, Input, PasswordInput, Stack } from "@mantine/core";
+import React, { useState, useCallback } from 'react';
+import { Box, Button, Center, Checkbox, Divider, Flex, Input, PasswordInput, Stack } from "@mantine/core";
 import classes from './SettingsBox.module.css';
-import { IconMessageReport, IconNotification, IconUser } from "@tabler/icons-react";
+import { IconMessageReport, IconUser } from "@tabler/icons-react";
 import { useDisclosure } from "@mantine/hooks";
+import { useAuth } from "@/hooks/AuthProvider";
+import { updatePassword } from "@/services/fetch";
+import bcrypt from "bcryptjs";
 
-export function SettingsBox(){
+export function SettingsBox() {
+    const [currentPassword, setCurrentPassword] = useState<string>("");
+    const [newPassword, setNewPassword] = useState<string>("");
+    const { user } = useAuth();
     const [visible, { toggle }] = useDisclosure(false);
-    
-    return(
-        <>
-        <Box bg="transparent" h="80vh" bd="1px solid myPurple.1" w="100%"  style={{
-            borderRadius: '83px 0 25px 25px'
-        }} >
-            <Center className={classes.banner}></Center>
+
+    const checkPassword = useCallback(async (password: string) => {
+        return await bcrypt.compare(password, user.password);
+    }, [user]);
+
+    const handleChangePassword = useCallback(async () => {
+        try {
+            const isCurrentPasswordValid = await checkPassword(currentPassword);
+            if (!isCurrentPasswordValid) {
+                console.error("Current password is incorrect");
+                return;
+            }
+            await updatePassword(user, newPassword);
+            console.log("Password changed successfully");
+        } catch (error) {
+            console.error("Error changing password", error);
+        }
+    }, [currentPassword, newPassword, checkPassword]);
+
+    return (
+        <Box bg="transparent" h="80vh" style={{
+            border: '1px solid var(--mantine-color-myPurple-1)',
+            borderRadius: '83px 0 25px 25px',
+            width: '100%'
+        }}>
+            <Center className={classes.banner} />
             <Divider size="xs" color="myPurple.1" />
-                
-            <Input variant="unstyled" size="lg" placeholder="Mi perfil" leftSection={<IconUser size={25} color="white"/>} />
+            
+            <Input readOnly variant="unstyled" size="lg" placeholder="Mi perfil" leftSection={<IconUser size={25} color="white" />} />
             <Divider size="xs" color="myPurple.1" />
 
-            <Input variant="unstyled" size="lg" ml={60} placeholder="Cambiar contraseña" />
+            <Input readOnly variant="unstyled" size="lg" ml={60} placeholder="Cambiar contraseña" />
             <Divider size="xs" color="myPurple.1" />
-
+            
             <Stack m={20} mb={50}>
                 <PasswordInput
                     w={220}
@@ -27,41 +53,53 @@ export function SettingsBox(){
                     label="Contraseña actual"
                     styles={{
                         label: {
-                        color: 'white',
-                        fontSize: '15px'
+                            color: 'white',
+                            fontSize: '15px'
                         }
                     }}
-                    defaultValue="secret"
                     visible={visible}
                     onVisibilityChange={toggle}
+                    onChange={(e) => setCurrentPassword(e.target.value)}
                 />
-                <PasswordInput
-                    w={220}
-                    ml={100}
-                    styles={{
-                        label: {
-                        color: 'white',
-                        fontSize: '15px'
-                        }
-                    }}
-                    label="Nueva contraseña"
-                    defaultValue="secret"
-                    visible={visible}
-                    onVisibilityChange={toggle}
-                />
+                <Flex justify="space-between" align="flex-end">
+                    <PasswordInput
+                        w={220}
+                        ml={100}
+                        styles={{
+                            label: {
+                                color: 'white',
+                                fontSize: '15px'
+                            }
+                        }}
+                        label="Nueva contraseña"
+                        visible={visible}
+                        onVisibilityChange={toggle}
+                        onChange={(e) => setNewPassword(e.target.value)}
+                    />
+                    <Button onClick={handleChangePassword} variant="filled" size="md" px="50" radius="xl" color="#483D8B">Enviar</Button>
+                </Flex>
             </Stack>
-            <Divider size="xs" color="myPurple.1" />
+            
+            <Divider size="xs" color="myPurple.1" mt={30} />
 
-            <Input variant="unstyled" size="lg" placeholder="Configuracion" leftSection={<IconMessageReport size={25} color="white"/>} />
+            <Input readOnly variant="unstyled" size="lg" placeholder="Configuracion" leftSection={<IconMessageReport size={25} color="white" />} />
             <Divider size="xs" color="myPurple.1" />
-
+            
             <Checkbox
                 defaultChecked
-                label="I agree to sell my privacy"
+                ml={50}
+                mt={10}
+                size="md"
+                styles={{
+                    label: {
+                        color: 'white'
+                    }
+                }}
+                label="Recibir notificaciones de mis recordatorios"
+                color="white"
+                iconColor="#191970"
             />
-            
+            <Button onClick={handleChangePassword} variant="filled" size="md" px="50" radius="xl" color="#483D8B">Enviar</Button>
         </Box>
-      
-        </>
-    )
+    );
 }
