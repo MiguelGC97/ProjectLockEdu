5d6569fed468f0905966f2c94e522e31a5311350
Yamizs <yamihego99gmail.com>
2025-02-20 14:31:04 +0000

    setup

---
+++ frontend/src/pages/ForgotPassword.js
@@ -0,0 +1,13 @@
+import React, { useState } from 'react';
+
+function ForgotPassword(){
+    return(
+        <div class="login-container">
+              <h2>Forgot Password</h2>
+
+        </div>
+      
+    )
+
+}
+export default ForgotPassword;
